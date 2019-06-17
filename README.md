ce869f4fc308d7f7752e82ce7b0e72093e8e5285
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2019-06-17 23:42:12 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Algorithm_and_Data_Structure
+
+## 2.Algorithm and computation time
+
+## 3.Elementary sort
+
+## 4.Data structure
+
+## 5.Search
--- src/5_search/iterator_STL.cpp
+++ src/5_search/iterator_STL.cpp
@@ -8,6 +8,11 @@
 void print(std::vector<int> v) {
     // ベクタの先頭から順番にアクセス
     std::vector<int>::iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        std::cout << *it;
+    }
+
+    std::cout << std::endl;
 }
 
 int main() {
@@ -19,6 +24,13 @@ int main() {
         std::cin >> x;
         v.push_back(x);
     }
+
+    print(v);
+
+    std::vector<int>::iterator it = v.begin();
+    *it = 3; // 先頭の要素 v[0] を 3 にする
+    it++; // 一つ前に進める
+    (*it)++; // v[1]の要素に 1 加算する
 
     print(v);
 
